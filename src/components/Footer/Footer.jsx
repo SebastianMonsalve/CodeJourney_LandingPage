9f3b72239e79ed5357c5e588dfb03eb31eeb26e7
Sebastian Monsalve <sebastiandev16@gmail.com>
2024-06-07 14:08:58 -0500

    fix to error

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import "./footer.css";
+import "./Footer.css";
 
 const footer = () => {
   const { t } = useTranslation();
